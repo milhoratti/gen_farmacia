5ba4235fb64fcdec78231cac21ff9d2d9b73ff53
Victor Milhoratti <92774535+milhoratti@users.noreply.github.com>
2022-07-05 17:55:46 -0300

    feat: farmacia files via upload

---
+++ farmacia/src/test/java/com/generation/farmacia/GenFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
